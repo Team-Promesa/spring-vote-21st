885d779a870694fb97f8d2fc143b5dc2d1b617b7
mirupio <162397816+mirupio@users.noreply.github.com>
2025-05-22 21:53:51 +0900

    initial commit

---
+++ src/test/java/com/ceos21/vote/VoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceos21.vote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
